a8214176c4a8c4baa16f69026cd0bcac71b0cfba
Kurt <schubert.geschaeftlich@gmail.com>
2025-01-23 10:19:27 +0100

    Create BTN to open a new window for the simulation. Create SimulationView.vue
    # Conflicts:
    #	ProofOfConcept_Timeline/src/assets/main.css

--- ProofOfConcept_Timeline/src/views/HomeView.vue
+++ ProofOfConcept_Timeline/src/views/HomeView.vue
@@ -1,7 +1,20 @@
 <script setup lang="ts">
 import Timeline from "@/components/Timeline.vue";
+import {useRouter} from "vue-router";
+
+// open Simulation
+const router = useRouter();
+const openSimulation = () => {
+  const newRoute = router.resolve({ name: 'simulation' });
+  const newWindowUrl = `${window.location.origin}${newRoute.href}`;
+  window.open(newWindowUrl, '_blank', 'width=500,height=500,resizable=yes,scrollbars=yes');
+};
+
 </script>
 
 <template>
   <Timeline></Timeline>
+
+  <button class="simbtn" @click="openSimulation">Open Simulation</button>
+
 </template>
