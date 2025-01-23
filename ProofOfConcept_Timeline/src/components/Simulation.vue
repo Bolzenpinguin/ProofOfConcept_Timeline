84c08c9d59be09fc75e55f959d417b2cc7c21541
Kurt <schubert.geschaeftlich@gmail.com>
2025-01-23 09:48:24 +0100

    Move CSS Code from Timeline.vue to main.css

---
+++ ProofOfConcept_Timeline/src/components/Simulation.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div>
+    <b-button v-b-toggle.sidebar-no-header >Toggle Sidebar</b-button>
+    <b-sidebar id="sidebar-no-header" aria-labelledby="sidebar-no-header-title" no-header shadow>
+      <template #default="{ hide }">
+        <div class="p-3">
+          <h4 id="sidebar-no-header-title">Custom header sidebar</h4>
+          <p>
+            Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis
+            in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
+          </p>
+          <b-button variant="primary" block @click="hide">Close Sidebar</b-button>
+        </div>
+      </template>
+    </b-sidebar>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
