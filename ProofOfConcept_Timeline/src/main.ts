8a371824f7377ec6a4603e0c790c1cd9ee52a0e5
Kurt <schubert.geschaeftlich@gmail.com>
2025-01-23 14:28:03 +0100

    Add FontAwesomeIcons to the project
    # Conflicts:
    #	ProofOfConcept_Timeline/src/main.ts

--- ProofOfConcept_Timeline/src/main.ts
+++ ProofOfConcept_Timeline/src/main.ts
@@ -5,9 +5,18 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import {faCaretDown, faCaretUp} from '@fortawesome/free-solid-svg-icons'
+
+// Up & Down cause of the text transformation
+library.add(faCaretDown, faCaretUp)
+
 const app = createApp(App)
 
 app.use(store);
-app.use(router);
+app.use(router)
 
-app.mount('#app');
+app
+    .component('font-awesome-icon', FontAwesomeIcon)
+    .mount('#app')
--- ProofOfConcept_Timeline/src/views/HomeView.vue
+++ ProofOfConcept_Timeline/src/views/HomeView.vue
@@ -2,6 +2,7 @@
 import Timeline from "@/components/Timeline.vue";
 import {useRouter} from "vue-router";
 
+
 // open Simulation
 const router = useRouter();
 const openSimulation = () => {
@@ -15,6 +16,36 @@ const openSimulation = () => {
 <template>
   <Timeline></Timeline>
 
-  <button class="simbtn" @click="openSimulation">Open Simulation</button>
+
+  <div class="btnsim-container" >
+    <button class="simbtn" @click="openSimulation">
+      <font-awesome-icon :icon="['fas', 'caret-down']" />
+      Open Simulation
+      <font-awesome-icon :icon="['fas', 'caret-down']" />
+    </button>
+  </div>
 
 </template>
+
+<style scoped>
+.simbtn {
+  margin: 16px;
+  justify-content: center;
+  align-content: center;
+  background: #EC660C;
+  color: white;
+  border-radius: 10px;
+  border-style: none;
+  font-weight: 600;
+  font-size: 16px;
+  transform-origin: center;
+  display: inline-block;
+  position: fixed;
+  right: 0;
+  top: 50%;
+  transform: translate(50%, -50%) rotate(90deg);
+  padding: 15px 20px;
+  cursor: pointer;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+}
+</style>
