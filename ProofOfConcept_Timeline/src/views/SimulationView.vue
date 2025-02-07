906a44245b0ef60f2c994cd03d9c6e9260b1dfa1
Kurt <schubert.geschaeftlich@gmail.com>
2025-02-07 10:45:41 +0100

    Refactor code and remove wrong loading functions
    Now only obj format is supported
    
    Wrap Simulation in conatiner
    
    install d3-d3 package

--- ProofOfConcept_Timeline/src/views/SimulationView.vue
+++ ProofOfConcept_Timeline/src/views/SimulationView.vue
@@ -2,10 +2,8 @@
 import * as THREE from "three";
 import { onMounted } from "vue";
 import Stats from "three/addons/libs/stats.module.js";
-import { GUI } from "three/addons/libs/lil-gui.module.min.js";
 import { OBJLoader } from "three/addons/loaders/OBJLoader.js";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
-import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
 
 // Initialize Actor Models
 let actorModel;
