5e53fa452e913ddc539309735e118358ee9542ca
Kurt <schubert.geschaeftlich@gmail.com>
2025-03-11 11:14:25 +0100

    Update from Chris & update path to JSON File in Timeline.vue

--- ProofOfConcept_Timeline/src/components/Timeline.vue
+++ ProofOfConcept_Timeline/src/components/Timeline.vue
@@ -9,7 +9,7 @@ import Grid from "@/components/Grid.vue";
 import PlaybackIndicator from "@/components/PlaybackIndicator.vue";
 
 import config from "@/config";
-import JsonData from '../json/2024-06-20_Team1_session1.json';
+import JsonData from '/src/json/2024-06-20_Team1_session1.json';
 import PlaybackVisualization from "@/components/PlaybackVisualization.vue";
 import Slider from "@/components/Slider.vue";
 import store, {type BlockSelection} from "@/store";
@@ -308,4 +308,56 @@ export default defineComponent({
      text-transform: uppercase;
    }
 
+   .playbackContainer button:hover {
+     color: #EC660C;
+     background-color: rgba(236, 102, 12, 0.12);
+     border-style: solid;
+     border-color: #EC660C;
+   }
+   
+   .playbackContainer button:active {
+     color: #EC660C;
+     background-color: white;
+     border-style: solid;
+     border-color: #EC660C;
+   }
+
+   .playbackContainer button:disabled {
+     color: white;
+     background-color: rgba(75, 75, 75, 0.49);
+   }
+
+   .playbackContainer button:disabled:hover {
+     border-style: none;
+     cursor: default;
+   }
+   
+   .playbackContainer select {
+     width: 164px;
+     font-weight: 600;
+     font-size: 16px;
+     letter-spacing: 0.1rem;
+     border-radius: 6px;
+     border-style: none;
+     color: white;
+     background-color: #EC660C;
+     text-transform: uppercase;
+     padding-left: 8px;
+   }
+
+   .playbackContainer select option {
+     width: 148px;
+     font-weight: 600;
+     font-size: 16px;
+     letter-spacing: 0.1rem;
+     border-radius: 6px;
+     border-style: none;
+     color: #EC660C;
+     background-color: white;
+   }
+
+   .playbackContainer select option:checked {
+     color: white;
+     background: #EC660C;
+   }
 </style>
--- ProofOfConcept_Timeline/src/main.ts
+++ ProofOfConcept_Timeline/src/main.ts
@@ -8,6 +8,15 @@ import store from './store'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import {faCaretDown, faCaretUp} from '@fortawesome/free-solid-svg-icons'
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({
+    components,
+    directives,
+});
 
 // Up & Down cause of the text transformation
 library.add(faCaretDown, faCaretUp)
@@ -16,6 +25,7 @@ const app = createApp(App)
 
 app.use(store);
 app.use(router)
+app.use(vuetify);
 
 app
     .component('font-awesome-icon', FontAwesomeIcon)
